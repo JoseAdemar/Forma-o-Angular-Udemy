d4253d9f1ecd76f0f0e5dcc2ce0d571d6ed01519
joseademar <joseademarti@gmail.com>
2023-03-30 22:06:16 -0300

    Aprendendo a criar components e também realização do primeiro desafio

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { successAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertCompoent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertCompoent,
+    successAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
