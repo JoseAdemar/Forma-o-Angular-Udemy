a090e1e9cff254d4e4779a1c651a639341c1f5bf
joseademar <joseademarti@gmail.com>
2023-05-28 14:08:27 -0300

    Estudando diretivas do angular

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -2,14 +2,24 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
 
   serverId: number = 10;
-  serverStatus: string = 'offline';
+  serverStatus: string = 'Status OffLine';
+  
+
+  constructor(){
+    this.serverStatus = Math.random() > 0.5 ? 'Status Online' : 'Status OffLine';
+  }
 
   getServerStatus(){
     return this.serverStatus;
+  }
+
+  getColor(){
+    return this.serverStatus === 'Status Online' ? 'green' : 'red';
   }
 }
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus  = 'No sever was created"'
   serverName = 'digite aqui';
+  serveCreated = false;
 
   constructor(){
     setTimeout(()=>{
@@ -21,6 +22,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
+    this.serveCreated = true;
     this.serverCreationStatus = 'The serve was created!';
   }
 
