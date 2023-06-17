37b22851d787145f5c1e4776884fbac40bf5971b
joseademar <joseademarti@gmail.com>
2023-06-17 13:01:49 -0300

    Criando os componentes do projeto

--- my-book-project/my-recipe-book-app/src/app/app.module.ts
+++ my-book-project/my-recipe-book-app/src/app/app.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ReceipeListComponent } from './recipes/receipe-list/receipe-list.component';
+import { ReceipeDetailComponent } from './recipes/receipe-detail/receipe-detail.component';
+import { RecipeItemComponent } from './recipes/receipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ReceipeListComponent,
+    ReceipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+  
   ],
   imports: [
     BrowserModule
@@ -13,4 +30,6 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+}
